f52c26b5fd59941dbd3ef1556054c7630c58dec0
Stefan <stefan.badertscher1@gmail.com>
2018-04-08 16:39:20 +0200

    folder structure

--- README.md
+++
@@ -1,4 +0,0 @@
-# d3.js-sandbox
-
-Simple D3.js snippets for getting started with data visualisation.
-Examples work without a webserver unless otherwise stated
--- data.js
+++
@@ -1,9 +0,0 @@
-var data = [];
-
-for (var i = 0; i < 25; i++) {
-  //  var num = Math.floor(Math.random() * 50);
-  var num = Math.floor(d3.randomUniform(1, 50)());
-  data.push(num);
-}
-
-console.log(data);
